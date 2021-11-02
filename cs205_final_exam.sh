451cbfaca9e612fcd8cc4ca4fdd41792e756785e
Cameron Montesano <ckmontesano@gmail.com>
2021-11-01 22:26:27 -0700

    modified script as requested and included screenshot of output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,33 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+
+awk -v filename=$1 '
+BEGIN { FS="\t" }
+
+{
+	if (NR!=1) {
+		pokemoncount += 1
+		totalhp += $6
+		totalattack += $7
+	}
+}
+
+END {
+	printf "======= SUMMARY OF %s =======\n", filename
+	printf "   Total Pokemon: %d\n", pokemoncount
+	printf "   Avg. HP: %d\n", totalhp/pokemoncount
+	printf "   Avg. Attack: %d\n", totalattack/pokemoncount
+	print "======= END SUMMARY =======\n"
+}
+
+' $1
+
+# Code Explanation (#s are lines)
+# 14 -> Call awk with given argument (filename)
+# 15 -> Declare \t as delimeter for input file
+# 18 -> For each line in file...
+# 19 through 21 -> Add to sum for pokemon count, total hp, and totalattack
+# 26 through 30 -> Display summary of input file
+# 33 -> Call awk with $1, which is the input filename
