66fbc615624409674f621f7ef83b3a644e96e70c
michaelchestnut <mwc72@nau.edu>
2021-11-01 20:02:46 -0700

    saving changes from the .sh file

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,9 +1,34 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ======= SUMMARY OF POKEMON.DAT ======
 #    Total Pokemon: [VALUE]
 #    Avg. HP: [VALUE]
 #    Avg. Attack: [VALUE]
 # ======= END SUMMARY =======
+
+
+#positional parameter for entered file
+
+FILE=$1
+
+
+#echo outputs the entered string
+
+echo "======= SUMMARY OF POKEMON.DAT ======"
+
+#finds and prints number of pokemon
+awk 'END{print "    Total Pokemon: ", NR}' $FILE
+
+
+#finds and prints average HP
+awk '{sum+=$6}END{print "    Avg. HP: ",sum/NR}' $FILE
+
+#finds and prints average attack
+awk '{sum+=$7}END{print "    Avg. Attack: ",sum/NR}' $FILE
+
+#echo outputs entered string
+echo "======= END SUMMARY ======="
+
+
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
