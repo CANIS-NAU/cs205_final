a5b9ee9a772f3af39045988a57f9b0d2a5a00e22
N1ckP3rsl3y <72055903+N1ckP3rsl3y@users.noreply.github.com>
2021-11-02 17:37:31 -0700

    Updated shell script to get info from pokemon.dat

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,28 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+hp=0
+sum=0
+attack=0
+
+echo "======= SUMMARY OF POKEMON.DAT ======"
+
+IFS=$'\n'
+awk 'BEGIN{FS="\t"}
+{
+	if (NR!=1){
+	hp+=$5
+
+	sum+=1
+
+	attack+=$6
+	}
+}
+END{
+print "   Total Pokemon: " sum
+print "   Avg. HP: " (hp/sum)
+print "   Avg. Attack: " (attack/sum)
+}' pokemon.dat
+
+echo "======= END SUMMARY ======="
