339eb0d6f1781393a4a70c55b5874f3023a7d97b
davidknight00 <davidmlknight@gmail.com>
2021-11-03 22:15:36 -0700

    Modified the sh file to display the total number of pokemon as well as display the average hp and attack stats with strings letting the user know which line relays what info. A new directory was also added holding a screenshot of the output of the file.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,12 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+# using awk, go to the end of the file and capture the last value in the first column
+awk 'END{print "Total Pokemon: " NR}' pokemon.dat
+
+# using awk, start at the beginning and go to the end getting a total of all the hp stats, then divide by a running total of the number of pokemon
+awk 'BEGIN{FS="\t"}{avg += $6 / (800)}END{print "Avg. HP: " avg}' pokemon.dat
+
+# repeat the same step as the last command but use the values in the attack stat column
+awk 'BEGIN{FS="\t"}{avg += $7 / (800)}END{print "Avg, Attack: " avg}' pokemon.dat
