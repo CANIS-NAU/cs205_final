b2a627eb7ff6dc5178af84c5957c53be12862bf4
jg3425 <jg3425@nau.edu>
2021-11-03 21:32:50 -0700

    Modified cs205_final_exam.sh to print summary of pokemon.dat and added screenshots directory and capture.png which shows the output of the shell script

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,27 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!bin/bash
+
+FILE=$1 # Set file name to first argument passed 
+
+if [ -e $FILE ] # If the file exists then continues otherwise exit
+then
+	# Prints in the beginning of the formatted summary
+	awk 'BEGIN{print "======= SUMMARY OF POKEMON.DAT ======"}' $FILE
+
+	# Print the total pokemon by taking line total and subtracting one for beginning line
+	awk 'END{print "   Total Pokemon:", NR-1}' $FILE
+
+	# Prints the health average by taking sum of every hp from column 6 and then dividing that by the total pokemon
+	awk '{hpsum+=$6;}END{print "   Avg. HP:", hpsum/(NR-1)}' $FILE
+
+	# Prints the attack average by taking the sum of every attack from column 7 and then dividing that by the total pokemon
+	awk '{atksum+=$7;}END{print "   Avg. Attack:", atksum/(NR-1)}' $FILE
+
+	# Print the end of the formatted summary 
+	awk 'BEGIN{print "======= END SUMMARY ======="}' $FILE
+fi
+
+
