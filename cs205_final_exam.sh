706776db4f75bff9565f8d8c3b71a61280f4b13f
Georgia Buchanan <georgia_hope@hotmail.com>
2021-11-04 14:50:05 -0700

    Took me a minute but this was fun! - Georgia Buchanan

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,31 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+# Buchanan
+# Georgia
+# ghb23
+# cs205_final
+
+#!/bin/bash
+
+#while read -r line; do
+#	hpTotal=$6
+#done < $FILE
+
+awk 'BEGIN{FS="\t"}
+{
+if(NR!=1){
+hp+=1
+hpTotal+=$6
+attack+=$7
+sum+=1
+}
+mean1=hpTotal/sum
+mean2=attack/sum
+}
+END {
+print "Total Pokemon: "sum
+print "Average HP: "mean1
+print "Average Attack: "mean2
+}' pokemon.dat
