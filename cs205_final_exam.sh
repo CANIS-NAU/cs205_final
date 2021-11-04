08960eada44625ecc8011fb860c349503f3a3ea9
Liam Scholl <liamscholl63@gmail.com>
2021-11-04 15:19:01 -0700

    Modified .sh file and added screenshot subdirectory containing screenshot of output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,11 +1,10 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ======= SUMMARY OF POKEMON.DAT ======
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ======= END SUMMARY =======
-
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+#!/bin/bash
+# Get positional parameter
+FILE=$1
+echo "======= SUMMARY OF POKEMON.DAT ======"
+# One liner awk, -F specifies the tab delimeter the file has
+# NR!=1 specifies to skip the first line in the file (since it's headers)
+# and it is here that total line count, hp, and attack is added up
+# END prints out the results in the correct format
+awk -F'\t' 'NR!=1{hp+=$6;att+=$7;line_count+=1;} END{print "Total Pokemon: " line_count; print "Avg. HP: " hp / line_count; print "Avg. Attack: " att / line_count}' ${FILE}
+echo "======= END SUMMARY ======="
