4d396f38dd834926f6ae682c34d0f7afef5abdc2
Solarias2 <82850976+Solarias2@users.noreply.github.com>
2021-11-04 21:42:45 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,30 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#HP and Atk locations in the file. This registers what each line what needs to be look for when these are called.
+hp=$6
+atk=$7
+
+#Formulas in which the program would calculate the total of hp and atk before division
+totalHP+=hp
+totalATK+=atk
+
+#counts the total number of lines that the file contains
+lines+=1
+}
+#Calculation of average HP and average Atk based on the number of pokemon that currently exists
+averageHP=totalHP/lines
+averageATK=totalATK/lines
+
+#Print statements that display the graph and information requested once shell script has been executed.
+print "======= SUMMARY OF POKEMON.DAT ======"
+print "    Total Pokemon: $lines"
+print "    Avg. HP: $averageHP"
+print "    Avg. Attack: $averageATK"
+print "======= END SUMMARY ======="
+
+}
+END{}
