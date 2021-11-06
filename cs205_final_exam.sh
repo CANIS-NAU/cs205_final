036c31729ab5c4541b3b3ff7c7958a06622a3083
Michael Torres <mjt378@nau.edu>
2021-11-06 15:27:23 -0700

    Completed the cs205_final_exam file and added screenshots directory with picture.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,30 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+sum+=1
+}
+}
+END{
+print "Total Pokemon: " sum}
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+hp+=$6
+}
+}
+END{print "Avg. HP: " hp/sum}
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+at+=$7
+}
+}
+END{print "Avg. Attack: " at/sum}
+
+
