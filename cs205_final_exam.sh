09f2637be8ab138cd45ebd3acaba771e02b755cb
Tim-Singleton <tas626@nau.edu>
2021-11-06 11:12:15 -0700

    Final changes

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,8 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+awk 'END{print "Total Pokemon: " $1}' pokemon.dat
+awk '{AvgHP += $6} END{print "Avg. HP: " AvgHP / NR}' pokemon.dat
+awk '{AvgAttack += $7} END{print "Avg. Attack: " AvgAttack / NR}' pokemon.dat
+
