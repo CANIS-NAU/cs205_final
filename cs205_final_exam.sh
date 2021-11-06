e8698163ad6eefc4ef35a9c7551e1e234b707ccc
dumspironeco <joerger.cameron@gmail.com>
2021-11-05 18:54:55 -0700

    edited script file and added screenshot per instructions

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,21 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#TODO change HP to add up the number of HP on the Pokemon listed
+hp+=$6
+attack+=$7
+
+#Counts the number of lines in the file
+sum+=1
+}
+}
+END{
+# Prints the number of lines, number of pokemon in the file and average of HP 
+print "Total Pokemon: " sum
+print "Average HP " hp/sum
+print "Average Attack: " attack/sum
+}
