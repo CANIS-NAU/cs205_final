113e05ca346f38f997bef8fe47c218763bb88a84
clm693 <93413915+clm693@users.noreply.github.com>
2021-11-06 13:21:19 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,19 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS="\t"}
+{
+if (NR!=1)
+hp+=1
+sum+=$6
+sum1+=$7
+}
+}
+END{
+print "======= SUMMARY OF POKEMON.DAT ======"
+print "   Total Pokemon: ", NR-1
+print "   Avg. HP: ", sum/(NR-1) 
+print "   Avg. Attack: " sum1/(NR-1)
+print " ======= END SUMMARY ======="
+}
