45c257ef6d12efd6843e87fb3d3845ef9394c60b
Joshua-Melo <jop22@nau.edu>
2021-11-06 01:19:05 +0000

    modified ssh file to run script, scp'd screenshot into /screenshots folder

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+{
+	hp+= $5;
+	attack+= $6;
+	line++
+	hp_mean = hp/line;
+	attack_mean = attack/line;
+}
+END{
+print hp_mean
+print attack_mean
+print line
+}
