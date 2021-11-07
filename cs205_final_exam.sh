845093a715757126d1529719aed7a6f22e37af8b
Brian Ruiz <bsr92@nau.edu>
2021-11-07 13:23:16 -0700

    Modified the exam file to print the total number of pokemon, average number of hp, and average number of attack points.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,24 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{NR="\t"}
+{
+# checks that the number of records is not 1
+	if (NR!=1){
+		# adds up all of the hp
+		hp+=$6
+		# adds up all of the attack points
+		attack$=$7
+		# variable to hold the total number of pokemon
+		sum+=1
+	}
+}
+END{
+# prints the output specified by the instructions
+print "======= SUMMARY OF POKEMON.DAT ======\n"
+print "   Total Pokemon: " sun " \n"
+print "   Avg. HP: " hp/sum " \n"
+print "   Avg. Attack: " attack/sum " \n"
+print "======= END SUMMARY =======\n"
+}
