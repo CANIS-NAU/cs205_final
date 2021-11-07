2f264f44653ee45c9907414ce710e78a48272fe3
DylanAnderson35 <89941454+DylanAnderson35@users.noreply.github.com>
2021-11-07 15:42:55 -0700

    Changed file to display count of pokemon, hp average, and attack

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -8,4 +8,13 @@
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted
+FILE=$1
+COUNTER=0
+while read i; do
+	let COUNTER++
+done < "$FILE"
+let COUNTER=COUNTER-1
+echo "Total Pokemon: $COUNTER"
+echo "Avg. HP:"
+echo "Avg. Attack:"
