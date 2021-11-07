4bf6cb53d595fb1e150b4c2a91dea865118df5dd
Schrixx <fun_nos@yahoo.com>
2021-11-06 21:11:56 -0700

    Created program listing a report and a screenshot of the output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,10 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+awk read pokemon.dat
+exec < pokemon.dat
+count=0
+while read line
+do
+	count=`$count + 1`
+done
