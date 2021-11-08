425772b782424c37582df4caf2bc740c82f1a246
ccc467 <ccc467@nau.edu>
2021-11-08 06:44:04 +0000

    Created AWK file that takes in pokemon data and outputs stats and included screenshot of this working in the screenshots folder

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+FILE=$1
+
+TP=$(awk 'BEGIN{i=0}{i++}END{print i}' FS='\t' $FILE)
+AHP=$(awk '{sum += $6 }END{print sum / NR}' FS='\t' $FILE)
+AA=$(awk '{sum += $7 }END{print sum / NR}' FS='\t' $FILE)
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ======= SUMMARY OF POKEMON.DAT ======
 #    Total Pokemon: [VALUE]
@@ -9,3 +17,9 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+echo "======= SUMMARY OF POKEMON.DAT ======="
+echo "Total Pokemon: " $TP
+echo "Avg. HP: " $AHP
+echo "Avg. Attack: " $AA
+echo "======= END SUMMARY ======="
