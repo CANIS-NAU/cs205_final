9b82a63ab97a8be99ea998b360f793339cee9c75
Nate Hill <nathan.hill210@gmail.com>
2021-11-07 19:09:57 -0700

    On branch ndh89_fall2021  Changes to be committed: 	modified:   cs205_final_exam.sh 	new file:   screenshot/screen-2021-11-07-19-07-34.jpg
    Changed the cs205_final_exam.sh so it worked per instructions. Added a
    screen shot of the output in the newly created sceenshot dir.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,26 +1,12 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ======= SUMMARY OF POKEMON.DAT ======
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ======= END SUMMARY =======
-
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
 
 # Section Number: CS205 - 001
 # Last Name: Hill
 # First Name: Nathan 
 # NAU Username: ndh89
 
-BEGIN{ FS="\t"
+BEGIN{FS="\t"
 }
 {
-sum=0;
-hp=0;
-atk=0;
 startStr=" ======= SUMMARY OF POKEMON.DAT ======";
 endStr=" ======= END SUMMARY ======= ";
 totalStr="    Total Pokemon: ";
