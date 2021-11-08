b3c1cde819f9eb049940a4f4f9401ec91bd4b070
Isaiah <isaiahraspet@gmail.com>
2021-11-07 20:46:29 -0700

    implemented cs205_final_exam.sh to go through a file similar to pokemon .dat. The file is executed through ./cs205_final_exam.sh file, where file is a file similar to pokemon.dat

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -5,7 +5,27 @@
 #    Avg. Attack: [VALUE]
 # ======= END SUMMARY =======
 
+FILE=$1
+
+cat $FILE | awk '
+BEGIN {FS="[space]"}
+{
+  if(NR!=1) {
+    hp+=$5
+    atk+=$7
+    sum+=1
+    num = $1 + 0
+  }
+}
+END{
+  print "TOTAL POKEMON: " num
+  print "Avg. HP: " hp / sum
+  print "Avg. Attack: " atk / sum
+}
+'
+
+
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
+# There should be a comment explaining the purpose of each line in your shell script.
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
