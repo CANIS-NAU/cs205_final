d7a0348b9d6c6df629e0d7fcaddbb2f3fe187775
Jack <jfh67@nau.edu>
2023-03-31 11:40:57 -0700

    This is the file that is to be changed to perform the tasks given in the code.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,23 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#This is the code that prints the header
+echo "======= SUMMARY OF POKEMON.DAT ======"
+#This awk command gives the total number of pokemon, and discludes the header line
+awk 'END{print "Total Pokemon: ", NR-1}' pokemon.dat
+#This awk command gives the Avg. HP by adding all the HP values in $5 and dividing by number of Pokemon
+awk '{sum+=$5} END{print "Avg. HP: " sum/(NR-1)}' pokemon.dat
+#This awkcommand gives the Avg. Attack by doing what the previous line does but with $6
+awk '{sum_attack+=$6} END{print "Avg. Attack: " sum_attack/(NR-1)}' pokemon.dat
+#This line of code prints the footer line 
+echo "======= END SUMMARY ======="
+
+
+
+
+
+
+
+
+
