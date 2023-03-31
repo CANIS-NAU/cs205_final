0c5593d40d00f7705ed92f476fe22ec0c2071ca8
LunaticSage218 <113202587+LunaticSage218@users.noreply.github.com>
2023-03-31 10:31:22 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -5,6 +5,35 @@
 #    Avg. Attack: [VALUE]
 # ======= END SUMMARY =======
 
+#!/bin/bash
+
+# set the input file name
+input_file="pokemon.dat"
+
+# print top title line
+echo "======= SUMMARY OF POKEMON.DAT ======"
+
+# use awk to count the number of lines in the input file
+line_count=$(awk 'END{print NR}' $input_file)
+
+# since there are one pokemon per line, print the line count
+echo "Total Pokemon: $line_count"
+
+# use awk to calculate the average HP in the 23rd column
+HPaverage=$(awk '{sum+=$23} END {print sum/NR}' $input_file)
+
+# print the average to the console
+echo "Avg. HP: $HPaverage"
+
+# use awk to calculate the attack average
+ATTACKaverage=$(awk '{sum+=$33} END {print sum/NR}' $input_file)
+
+# print the average attack 
+echo "Avg. Attack: $ATTACKaverage"
+
+# print the bottom summary line
+echo "======= END SUMMARY ======="
+
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
